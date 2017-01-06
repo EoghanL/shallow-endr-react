5db77f26453988562b8e8e8e8c64f4d2fdf12cd3
marcimmediato <marc.immediato@gmail.com>
2017-01-06 13:09:21 -0500

    fixing issues with new artist information

--- src/components/artist_list.js
+++ src/components/artist_list.js
@@ -27,60 +27,60 @@ class ArtistList extends Component{
   }
 
   render() {
-  let artistArray = []
-  let newArtists = []
-  let existingArtists = []
-  let that = this
-  let newArtistsText= ""
-  let existingArtistsText = ""
-  let artistNameText = ""
-  if(this.props.artist !== null && this.props.artist !== undefined){
-    artistNameText = this.props.artist.display_name
-    artistArray = <Artist id={this.props.artist.id} name={this.props.artist.display_name} songs={this.props.songs} rankings={this.props.rankings} />
-  }
+    let artistArray = []
+    let newArtists = []
+    let existingArtists = []
+    let that = this
+    let newArtistsText= ""
+    let existingArtistsText = ""
+    let artistNameText = ""
+    if(this.props.artist !== null && this.props.artist !== undefined){
+      artistNameText = this.props.artist.display_name
+      artistArray = <Artist id={this.props.artist.id} name={this.props.artist.display_name} songs={this.props.songs} rankings={this.props.rankings} />
+    }
 
-  let artist_list = <Artist />
-  if (Array.isArray(this.props.newArtists) && this.props.newArtists.length > 0) {
-    artistNameText = ""
-    newArtistsText = "Do you want to add a new artist? Click name to add."
-    newArtists = this.props.newArtists.map(function(artist){
-      let dateInfo = ''
-      let year = artist.date_begin.split('-')[0]
-      let allmusicLink = ''
-      if (artist.urls.allmusic) { allmusicLink = <a target="_blank" href={artist.urls.allmusic}>Allmusic Page</a> }
+    let artist_list = <Artist />
+    if (Array.isArray(this.props.newArtists) && this.props.newArtists.length > 0) {
+      artistNameText = ""
+      newArtistsText = "Do you want to add a new artist? Click name to add."
+      newArtists = this.props.newArtists.map(function(artist){
+        let dateInfo = ''
+        let year = artist.date_begin.split('-')[0]
+        let allmusicLink = ''
+        if (artist.urls.allmusic) { allmusicLink = <a target="_blank" href={artist.urls.allmusic}>Allmusic Page</a> }
 
-      if (year != 2030) {
-        if (artist.type == "Person") {
-          dateInfo = `(Born: ${year})`
-        } else {
-          dateInfo = `(Formed: ${year})`
+        if (year != 2030) {
+          if (artist.type == "Person") {
+            dateInfo = `(Born: ${year})`
+          } else {
+            dateInfo = `(Formed: ${year})`
+          }
         }
-      }
-      return <li className="list" key={artist.id}><span className='new-artist-line' name={artist.name} id={artist.id} onClick={that.createNewArtist}>{artist.name} {dateInfo}</span>&nbsp;{allmusicLink}</li>
-    })
+        return <li className="list" key={artist.id}><span className='new-artist-line' name={artist.name} id={artist.id} onClick={that.createNewArtist}>{artist.name} {dateInfo}</span>&nbsp;{allmusicLink}</li>
+      })
 
-  if (Array.isArray(this.props.existingArtists) && this.props.existingArtists.length > 0) {
-    artistNameText = ""
-    existingArtistsText= "Did you mean? Click to select."
-    existingArtists = this.props.existingArtists.map(function(artist){
-      return <li className="list" key={artist.id} name={artist.name} id={artist.id} onClick={that.getExistingArtist}>{artist.display_name}</li>
-    })
+    if (Array.isArray(this.props.existingArtists) && this.props.existingArtists.length > 0) {
+      artistNameText = ""
+      existingArtistsText= "Did you mean? Click to select."
+      existingArtists = this.props.existingArtists.map(function(artist){
+        return <li className="list" key={artist.id} name={artist.name} id={artist.id} onClick={that.getExistingArtist}>{artist.display_name}</li>
+      })
+    }
+    return (
+      <div className="artist-list">
+        <h3>{artistNameText}</h3>
+        {artistArray}
+        <p>{existingArtistsText}</p>
+        <ul>
+        {existingArtists}
+        </ul>
+        <p>{newArtistsText}</p>
+        <ul>
+          {newArtists}
+        </ul>
+      </div>
+    )
   }
-  return (
-    <div className="artist-list">
-      <h3>{artistNameText}</h3>
-      {artistArray}
-      <p>{existingArtistsText}</p>
-      <ul>
-      {existingArtists}
-      </ul>
-      <p>{newArtistsText}</p>
-      <ul>
-        {newArtists}
-      </ul>
-    </div>
-  )
-}
 }
 
 function mapStateToProps(state) {
